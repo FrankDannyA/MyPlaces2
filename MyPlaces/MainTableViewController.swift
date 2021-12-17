2bebf8b063b195eb25c3d9b1c3666f80edbe01a6
Daniil <orgfrank@icloud.com>
2021-12-17 13:10:51 +0600

    Добавил иконки для приложений Настроил Launch Screen Настроил тестовое изображение ячейки

--- MyPlaces/MainTableViewController.swift
+++ MyPlaces/MainTableViewController.swift
@@ -32,11 +32,20 @@ class MainTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
         cell.imageView?.image = UIImage(named: restarauntNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
+        
         cell.textLabel?.text = restarauntNames[indexPath.row]
         
         return cell
     }
     
+    
+    //MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
 
     
     // MARK: - Navigation
