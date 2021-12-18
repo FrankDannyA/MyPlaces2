e300caed60a67544e666a3b98ccd8d4373d5d942
Daniil <orgfrank@icloud.com>
2021-12-18 20:55:14 +0600

    Добавил MapViewController

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Даниил Франк on 18.12.2021.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
